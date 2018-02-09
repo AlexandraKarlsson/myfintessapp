6633937d6f8a53eef36971da06247b9bbe030193
Alexandra Karlsson <betruebefree@gmail.com>
2018-02-09 19:05:01 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>Your fitness app</div>
+    );
+  }
+}
+
+export default App;
---
+++ src/hoc/layout/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Aux from '../auxiliary/Auxiliary';
+
+const layout = (props) => {
+    return(
+        <Aux>
+            <p>toolbar</p>
+        </Aux>
+    )
+};
+
+export default layout;
